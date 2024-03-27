2e0363318ce178efb22ba0d1655c6e6bb44be812
maryannemm <maryannemaree@gmail.com>
2024-03-27 09:53:00 -0700

    corrections

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -24,15 +24,3 @@ def find_peak(list_of_integers):
             left = mid + 1
 
     return list_of_integers[left]
-
-# Complexity: O(log(n))
-
-if __name__ == "__main__":
-    # Test cases
-    print(find_peak([1, 2, 4, 6, 3]))  # Output: 6
-    print(find_peak([4, 2, 1, 2, 3, 1]))  # Output: 3
-    print(find_peak([2, 2, 2]))  # Output: 2
-    print(find_peak([]))  # Output: None
-    print(find_peak([-2, -4, 2, 1]))  # Output: 2
-    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))  # Output: 4
-
